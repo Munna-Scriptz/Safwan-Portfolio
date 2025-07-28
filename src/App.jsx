118211da9ab6ab9add405592972f36f4e8795027
Munna-Scripts <munnascriptz@gmail.com>
2025-07-28 16:33:37 +0600

    Project Enviroment Setup Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { createRoutesFromElements, Route, RouterProvider } from 'react-router'
+import { createBrowserRouter } from 'react-router'
+import LayoutOne from './layout/LayoutOne'
+import Home from './pages/Home'
 
 const App = () => {
+
+  const MyRoute = createBrowserRouter(createRoutesFromElements(
+    <Route>
+      <Route path='/' element={<LayoutOne/>}>
+        <Route index element={<Home/>}></Route>
+      </Route>
+    </Route>
+  ))
+
   return (
     <>
-      
+      <RouterProvider router={MyRoute}></RouterProvider>
     </>
   )
 }
