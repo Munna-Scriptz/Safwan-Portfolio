8ee92529e3bcb90e6d4bd4d01b79ba656562c0bb
Munna-Scripts <munnascriptz@gmail.com>
2025-07-30 17:24:43 +0600

    Our Skill Complete

--- src/components/OurSkills.jsx
+++ src/components/OurSkills.jsx
@@ -6,7 +6,7 @@ const OurSkills = () => {
     <>
         <section id='Skills' className='mt-[140px]'>
             <div className="container">
-                <div id="AllDiv">
+                <div id="AllDiv" className='flex items-center justify-between'>
                     {/* ------------Left Side---------- */}
                     <div>
                         <p className='font-vollkorn font-medium text-[18px] text-[#EF8100]'>OUR SKILLS</p>
@@ -15,6 +15,32 @@ const OurSkills = () => {
                         <Link className='bg-[#E5745D] text-white py-[21px] px-[39px] font-jost text-[18px] border-2 border-[#fff] outline-4 outline-[#E5745D]' to={'/'}>Download Cv</Link>
                     </div>
                     {/* ------------Left Side---------- */}
+                    <div className='w-[670px] flex flex-col gap-[30px]'>
+                        <div>
+                            <p className='font-vollkorn font-medium text-[18px] text-primary flex items-center justify-between'>Adobe Illustration <span>60%</span></p>
+                            <div className='relative w-full h-[10px] bg-primary mt-[11px]'>
+                                <span className='absolute left-0 bottom-0 w-[427px] h-full bg-second'></span>
+                            </div>
+                        </div>
+                        <div>
+                            <p className='font-vollkorn font-medium text-[18px] text-primary flex items-center justify-between'>Adobe Photoshop <span>85%</span></p>
+                            <div className='relative w-full h-[10px] bg-primary mt-[11px]'>
+                                <span className='absolute left-0 bottom-0 w-[592px] h-full bg-second'></span>
+                            </div>
+                        </div>
+                        <div>
+                            <p className='font-vollkorn font-medium text-[18px] text-primary flex items-center justify-between'>Figma <span>90%</span></p>
+                            <div className='relative w-full h-[10px] bg-primary mt-[11px]'>
+                                <span className='absolute left-0 bottom-0 w-[645px] h-full bg-second'></span>
+                            </div>
+                        </div>
+                        <div>
+                            <p className='font-vollkorn font-medium text-[18px] text-primary flex items-center justify-between'>Adobe XD <span>70%</span></p>
+                            <div className='relative w-full h-[10px] bg-primary mt-[11px]'>
+                                <span className='absolute left-0 bottom-0 w-[528px] h-full bg-second'></span>
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
             </div>
