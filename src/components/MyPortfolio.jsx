558fc650128caa3f7e8d07a36d4d92e525adaa8c
Munna-Scripts <munnascriptz@gmail.com>
2025-07-30 16:55:08 +0600

    MyPortfolio Design Complete

--- src/components/MyPortfolio.jsx
+++ src/components/MyPortfolio.jsx
@@ -8,30 +8,63 @@ const MyPortfolio = () => {
                     <div id="TextHeader" className='mb-[40px]'>
                         <h2 className='font-vollkorn font-medium text-[50px] text-primary text-center'>My Working Portfolio</h2>
                     </div>
-                    <div id="projectShow">
-                        {/* ---------First Project------ */}
-                        <div className='w-[514px] h-[400px] outline-1 outline-[#00413D] flex items-center justify-center p-[30px]'>
-                            <div className='w-full h-full bg-[#C4C4C4]'>
-
-                            </div>
-                        </div>
-                        {/* ---------Second Project------ */}
-                        <div className='w-[514px] h-[660px] outline-1 outline-[#00413D] flex items-center justify-center p-[30px]'>
-                            <div className='w-full h-full bg-[#C4C4C4]'>
-
-                            </div>
-                        </div>
-                        {/* ---------Third Project------ */}
-                        <div className='w-[514px] h-[400px] outline-1 outline-[#00413D] flex items-center justify-center p-[30px]'>
-                            <div className='w-full h-full bg-[#C4C4C4]'>
-
-                            </div>
-                        </div>
-                    {/* ---------Fourth Project------ */}
-                    {/* ---------Fifth Project------ */}
-                    </div>
-
                 </div>
+                    <div id="projectShow" className='flex gap-[29px] justify-center'>
+                        {/* ----------------First Column------------- */}
+                        <div className='flex flex-col gap-[29px] mt-[120px]'>
+                            <div className='w-[514px] h-[400px] outline-1 outline-[#00413D] flex items-center justify-center p-[30px]'>
+                                <div className='w-full h-full bg-[#C4C4C4]'>
+
+                                </div>
+                            </div>
+                            <div className='w-[514px] h-[660px] outline-1 outline-[#00413D] flex items-center justify-center p-[30px]'>
+                                <div className='w-full h-full bg-[#C4C4C4]'>
+
+                                </div>
+                            </div>
+                            <div className='w-[514px] h-[400px] outline-1 outline-[#00413D] flex items-center justify-center p-[30px]'>
+                                <div className='w-full h-full bg-[#C4C4C4]'>
+
+                                </div>
+                            </div>
+                        </div>
+                        {/* ----------------Second Column------------- */}
+                        <div className='flex flex-col gap-[29px] mt-[40px]'>
+                            <div className='w-[514px] h-[660px] outline-1 outline-[#00413D] flex items-center justify-center p-[30px]'>
+                                <div className='w-full h-full bg-[#C4C4C4]'>
+
+                                </div>
+                            </div>
+                            <div className='w-[514px] h-[400px] outline-1 outline-[#00413D] flex items-center justify-center p-[30px]'>
+                                <div className='w-full h-full bg-[#C4C4C4]'>
+
+                                </div>
+                            </div>
+                            <div className='w-[514px] h-[660px] outline-1 outline-[#00413D] flex items-center justify-center p-[30px]'>
+                                <div className='w-full h-full bg-[#C4C4C4]'>
+
+                                </div>
+                            </div>
+                        </div>
+                        {/* ----------------Third Column------------- */}
+                        <div className='flex flex-col gap-[29px] mt-[120px]'>
+                            <div className='w-[514px] h-[400px] outline-1 outline-[#00413D] flex items-center justify-center p-[30px]'>
+                                <div className='w-full h-full bg-[#C4C4C4]'>
+
+                                </div>
+                            </div>
+                            <div className='w-[514px] h-[660px] outline-1 outline-[#00413D] flex items-center justify-center p-[30px]'>
+                                <div className='w-full h-full bg-[#C4C4C4]'>
+
+                                </div>
+                            </div>
+                            <div className='w-[514px] h-[400px] outline-1 outline-[#00413D] flex items-center justify-center p-[30px]'>
+                                <div className='w-full h-full bg-[#C4C4C4]'>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
             </section>
         </>
     )
